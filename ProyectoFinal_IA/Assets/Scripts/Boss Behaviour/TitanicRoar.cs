3c5152b38e27df49f649b1f7fa1696a4f69d2ee5
mihern07 <mihern07@ucm.es>
2022-05-22 14:01:48 +0200

    Rockbuster y Titanic Roar

---
+++ ProyectoFinal_IA/Assets/Scripts/Boss Behaviour/TitanicRoar.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class TitanicRoar : BossAction
+{
+    [SerializeField]
+    private GameObject aoeDisplayPrefab;
+
+    private GameObject aoeDisplayGameobject;
+
+    protected override void displayAoe()
+    {
+        castTime = 6;
+        actionTime = 4;
+        aoeDisplayGameobject = Instantiate<GameObject>(aoeDisplayPrefab);
+
+        //We show the castbar on the hud
+        GameManager.getInstance().startEnemyAbility(castTime, "Titanic Roar");
+
+        GetComponent<Animator>().SetBool("idle", false);
+        GetComponent<Animator>().SetBool("defy", true);
+
+        //The boss looks at the player while casting
+        GetComponent<LookAtPlayer>().setIsLooking(true);
+    }
+
+    protected override void doAction()
+    {
+        //Stops looking at player
+        GetComponent<LookAtPlayer>().setIsLooking(false);
+
+        GameManager.getInstance().playerLoseLife(5);
+
+        GetComponent<Animator>().SetBool("defy", false);
+        GetComponent<Animator>().SetBool("attack_03", true);
+
+        Destroy(aoeDisplayGameobject);
+    }
+
+    protected override void stopAction()
+    {
+    }
+}
